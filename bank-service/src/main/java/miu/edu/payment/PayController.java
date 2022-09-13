724a7db5b877b5a40cd7455edfc022c68b91a6ac
sainturb <saintur.batkhuu@miu.edu>
2022-09-13 14:52:24 -0500

    payment service added

---
+++ bank-service/src/main/java/miu/edu/payment/PayController.java
@@ -0,0 +1,22 @@
+package miu.edu.payment;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("api/pay")
+public class PayController {
+    @PostMapping
+    public Map<String, String> pay(@RequestBody Map<String, String> body) {
+        if (Objects.nonNull(body.get("account")) && Objects.nonNull(body.get("routing"))) {
+            return Map.of("response", "payment succeed");
+        } else {
+            return Map.of("error", "required fields are missing");
+        }
+    }
+}
---
+++ payment-service/src/main/java/miu/edu/payment/controllers/PaymentController.java
@@ -0,0 +1,40 @@
+package miu.edu.payment.controllers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("api/checkout")
+@Slf4j
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final ObjectMapper mapper;
+
+    @PostMapping
+    public Map<String, String> checkout(@RequestBody Map<String, Object> body, Principal principal) {
+        try {
+            Map<String, Object> map = mapper.readValue(principal.getName(), new TypeReference<HashMap<String,Object>>(){});
+            if (Objects.nonNull(map.get("payment"))) {
+
+            } else {
+                return Map.of("error", "payment method is not set");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Map.of("response", "checkout completed");
+    }
+}
