effa5df51372bb9dc5d54cdf72c49e3f47d5ee74
sainturb <saintur.batkhuu@miu.edu>
2022-09-17 18:08:25 -0500

    some fix

--- account-service/src/main/java/miu/edu/controller/ProfileController.java
+++ account-service/src/main/java/miu/edu/controller/ProfileController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 
 @RestController
-@RequestMapping("api/profile")
+@RequestMapping("api")
 @RequiredArgsConstructor
 @CrossOrigin
 public class ProfileController {
